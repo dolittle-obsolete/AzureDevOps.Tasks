d1e407f8f48ed75053619dfc0364bae2c88b642a
Sindre Wilting <sindrewilting@gmail.com>
2019-11-26 16:06:29 +0100

    Extend MockTestRunner with isMissingRequiredVariable

--- Tasks/UpdateVersion/for_running_task/when_there_is_no_version_input.ts
+++ Tasks/UpdateVersion/for_running_task/when_there_is_no_version_input.ts
@@ -4,12 +4,14 @@
  *--------------------------------------------------------------------------------------------*/
 import ttm = require('azure-pipelines-task-lib/mock-test')
 import path = require('path');
+import '@dolittle/azure-dev-ops.tasks.test-helpers';
 
 describe('when there is no version input', function () {
     let tp = path.join(__dirname, 'given', 'a_runner_without_version_input.js');
 
     let tr = new ttm.MockTestRunner(tp);
     tr.run();
-    
     it('should not succeed', () => tr.succeeded.should.be.false);
+    it('should have one error issue', () => tr.errorIssues.length.should.equal(1));
+    it('should be missing version variable', () => tr.isMissingRequiredVariable('version').should.be.true);
  });
--- TestHelpers/TestRunner.ts
+++ TestHelpers/TestRunner.ts
@@ -7,10 +7,15 @@ import ttm = require('azure-pipelines-task-lib/mock-test')
 declare module 'azure-pipelines-task-lib/mock-test' {
     export interface MockTestRunner {
         hasOutputVariable(variable: string, value: string, isSecret?: boolean): boolean
+        isMissingRequiredVariable(variable: string): boolean
     }
 }
 ttm.MockTestRunner.prototype.hasOutputVariable = function (variable: string, value: string, isSecret: boolean = false) {
     let self = this as ttm.MockTestRunner;
     return self.stdOutContained(`##vso[task.setvariable variable=${variable};issecret=${isSecret? 'true' : 'false'};]${value}`);
+};
 
+ttm.MockTestRunner.prototype.isMissingRequiredVariable = function (variable: string) {
+    let self = this as ttm.MockTestRunner;
+    return self.createdErrorIssue(`Input required:${variable}`);
 };
