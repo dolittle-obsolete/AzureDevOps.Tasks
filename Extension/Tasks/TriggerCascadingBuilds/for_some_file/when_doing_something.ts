fcdb273294aef8b2a2d6c03788c9fecbdcb876fa
Sindre Wilting <sindrewilting@gmail.com>
2019-11-28 12:34:41 +0100

    Change task definitions

---
+++ Extension/Tasks/TriggerCascadingBuilds/for_some_file/when_doing_something.ts
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { a_context } from "./given/a_context";
+
+describe('when doing something', () => {
+    let context = new a_context();
+    it('should do that', () => true.should.be.true)
+});
