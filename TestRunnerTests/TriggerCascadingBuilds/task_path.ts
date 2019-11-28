257db5693f246ff36f4a84d1b2402b733c81cf8b
Sindre Wilting <sindrewilting@gmail.com>
2019-11-28 21:16:09 +0100

    Setup test runner tests for TriggerCascadingBuild

---
+++ TestRunnerTests/TriggerCascadingBuilds/task_path.ts
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import {getPathToTask} from '@dolittle/azure-dev-ops.tasks.test-helpers';
+
+const task_path = getPathToTask('TriggerCascadingBuilds');
+export default task_path;
