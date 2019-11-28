002a272023795a68602e5d1eb25595f53899110f
Sindre Wilting <sindrewilting@gmail.com>
2019-11-28 22:01:20 +0100

    test runtter tests for TagNewVersion

---
+++ TestRunnerTests/TagNewVersion/for_repository_name/something.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import ttm = require('azure-pipelines-task-lib/mock-test')
+import path = require('path');
+import '@dolittle/azure-dev-ops.tasks.test-helpers';
+
+describe('something', () => {
+
+    it('something', () => true.should.be.true);
+ });
