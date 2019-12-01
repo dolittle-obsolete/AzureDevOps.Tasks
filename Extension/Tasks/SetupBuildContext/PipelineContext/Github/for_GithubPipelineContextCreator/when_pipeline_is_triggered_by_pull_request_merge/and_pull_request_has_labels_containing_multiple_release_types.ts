e86ee7cd46bc267c026a80475df5250488c10430
Sindre Wilting <sindrewilting@gmail.com>
2019-12-01 15:28:21 +0100

    Add specs

---
+++ Extension/Tasks/SetupBuildContext/PipelineContext/Github/for_GithubPipelineContextCreator/when_pipeline_is_triggered_by_pull_request_merge/and_pull_request_has_labels_containing_multiple_release_types.ts
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { expect } from 'chai';
+import { a_github_pipeline_context_creator } from '../given/a_github_pipeline_context_creator'
+import { PipelineContext } from '../../../PipelineContext';
+
+describe('and pull request has labels containing multiple release types', () => {
+    let context = new a_github_pipeline_context_creator();
+    let context_creator = context.pipeline_context_creator;
+    context.build_context.sourceBranchName = 'master';
+    (context.client.pulls as sinon.SinonStub).returns(Promise.resolve({data: [{
+        merge_commit_sha: context.build_context.sourceVersion,
+        labels: [{name: 'minor'}, {name: 'patch'}]
+    }]}));
+
+    let pipeline_context: PipelineContext
+    beforeEach(async () => {
+        pipeline_context = await context_creator.create(context.build_context, context.pull_request_context);
+
+    });
+    it('should not be undefined', () => expect(pipeline_context).to.not.be.undefined);
+    it('should publish', () => pipeline_context.shouldPublish.should.equal(true));
+    it('should have minor release type', () => pipeline_context.releaseType!.should.equal('minor'));
+    it('should have previous version 1.0.0', () => pipeline_context.previousVersion!.should.equal('1.0.0'));
+});
---
+++ Extension/Tasks/SetupBuildContext/PipelineContext/Github/for_GithubPipelineContextCreator/when_provider_is_not_GitHub.ts
@@ -0,0 +1,26 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { a_github_pipeline_context_creator } from "./given/a_github_pipeline_context_creator";
+import { expect } from "chai";
+
+
+describe('when provider is not GitHub', () => {
+    let context = new a_github_pipeline_context_creator();
+    let context_creator = context.pipeline_context_creator;
+
+    context.build_context.repositoryProvider = 'Something';
+
+    let exception: Error;
+    beforeEach(async () => {
+        try {
+            await context_creator.create(context.build_context, context.pull_request_context);
+        }
+        catch(error) {
+            exception = error;
+        }
+
+    });
+    it('should throw an exception', () => expect(exception).to.not.be.undefined );
+});
