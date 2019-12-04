e5dde66d11acdf50bc86d0ada826ca5135a27987
Sindre Wilting <sindrewilting@gmail.com>
2019-12-04 07:43:33 +0100

    Shared package

---
+++ Source/Extension/Shared/index.ts
@@ -0,0 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+export * from './ILogger';
+export * from './Logger';
+export * from './NullLogger';
