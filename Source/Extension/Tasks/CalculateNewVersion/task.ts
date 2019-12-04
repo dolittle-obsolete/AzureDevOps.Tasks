bd265b43513da0f66e186b2e5026cd5ccb1376c6
Sindre Wilting <sindrewilting@gmail.com>
2019-12-04 15:25:21 +0100

    Just pass through version if release type is undefined

--- Source/Extension/Tasks/CalculateNewVersion/task.ts
+++ Source/Extension/Tasks/CalculateNewVersion/task.ts
@@ -14,19 +14,27 @@ async function run() {
         const versionIncrementor = new VersionIncrementor();
 
         const previousVersion = taskLib.getInput('PreviousVersion', true)!;
-        const releaseType = taskLib.getInput('ReleaseType', true)! as ReleaseType;
+        const releaseType = taskLib.getInput('ReleaseType') as ReleaseType | undefined;
+        if (releaseType === undefined) {
+            console.log('Got undefined ReleaseType. Outputting PreviousVersion as NextVersion');
 
-        console.log(`Calculating new version from previous version '${previousVersion}' using release type '${releaseType}'`);
+            taskLib.setVariable('NextVersion', previousVersion);
+            taskLib.setResult(taskLib.TaskResult.Succeeded, 'No release type triggering new version');
+        }
+        else {
 
-        taskLib.debug(`Got Previous Version ${previousVersion}`);
-        taskLib.debug(`Got Release Type: ${releaseType}`);
-
-        taskLib.debug(`Updating version for new ${releaseType}`);
-        let newVersion = versionIncrementor.increment(previousVersion, releaseType);
-
-        console.log(`Setting next version to be '${newVersion}'`);
-        taskLib.setVariable('NextVersion', newVersion);
-        taskLib.setResult(taskLib.TaskResult.Succeeded, `Successfully updated version from '${previousVersion}' to ${newVersion}`);
+            console.log(`Calculating new version from previous version '${previousVersion}' using release type '${releaseType}'`);
+    
+            taskLib.debug(`Got Previous Version ${previousVersion}`);
+            taskLib.debug(`Got Release Type: ${releaseType}`);
+    
+            taskLib.debug(`Updating version for new ${releaseType}`);
+            let newVersion = versionIncrementor.increment(previousVersion, releaseType);
+    
+            console.log(`Setting next version to be '${newVersion}'`);
+            taskLib.setVariable('NextVersion', newVersion);
+            taskLib.setResult(taskLib.TaskResult.Succeeded, `Successfully updated version from '${previousVersion}' to ${newVersion}`);
+        }
     }
     catch (err) {
         taskLib.setResult(taskLib.TaskResult.Failed, err.message);
