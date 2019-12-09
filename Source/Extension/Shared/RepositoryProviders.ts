6e1cd0b644544f2e8dcacb1d7fbe41ff1a30fc02
Sindre Wilting <sindrewilting@gmail.com>
2019-12-09 09:55:14 +0100

    New shared ´

---
+++ Source/Extension/Shared/RepositoryProviders.ts
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+/**
+ * The repository providers that can trigger a build
+ *
+ * @export
+ * @class BuildProviders
+ */
+export class RepositoryProviders {
+    
+    static GitHub = 'GitHub'
+    
+} 
