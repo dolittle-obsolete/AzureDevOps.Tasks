a9069f9b65ed491ea245d3cbb4ef0ae514970106
Sindre Wilting <sindrewilting@gmail.com>
2020-01-13 12:35:49 +0100

    Remove unneeded code from TagNewVersion

--- Source/Extension/Tasks/TagNewVersion/Github/GithubRepositoryTagger.ts
+++ Source/Extension/Tasks/TagNewVersion/Github/GithubRepositoryTagger.ts
@@ -34,46 +34,6 @@ export class GithubRepositoryTagger implements ICanTagRepository {
             name: `Release version '${version}'`
         });
         this._logger.debug(`Status: ${releaseResponse.status}`);
-        // let commitSha = await this._getCommitSha();
-        // let tagObject = await this._createTagObject(tag, version, commitSha);
-        // await this._updateReference(tagObject.sha);
 
-    }
-
-    private async _getCommitSha() {
-        this._logger.debug(`Getting commit sha from '${this._owner}/${this._repo}'`);
-        const commitReference = await this._client.git.getRef({
-            owner: this._owner,
-            repo: this._repo,
-            ref
-        });
-        this._logger.debug(`Status: ${commitReference.status}`);
-        return commitReference.data.object.sha;
-    }
-
-    private async _createTagObject(tag: string, version: string, commitSha: string) {
-        this._logger.debug(`Creating tag object on '${this._owner}/${this._repo}'`);
-        const tagResponse = await this._client.git.createTag({
-            owner: this._owner,
-            repo: this._repo,
-            tag,
-            object: commitSha,
-            message: `Releasing version '${version}'`,
-            type: 'commit'
-        });
-        this._logger.debug(`Status: ${tagResponse.status}`);
-        return tagResponse.data;
-    }
-
-    private async _updateReference(tagSha: string) {
-        this._logger.debug(`Updating reference on '${this._owner}/${this._repo}'`);
-        const updateResponse = await this._client.git.updateRef({
-            owner: this._owner,
-            repo: this._repo,
-            ref,
-            sha: tagSha
-        });
-        this._logger.debug(`Status: ${updateResponse.status}`);
-        return updateResponse;
     }
 }
